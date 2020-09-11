fe735f0d2a9e4a3432a16f9afdd06e142551ef99
Robin Wong <robinwong76@hotmail.com>
2020-09-11 19:48:05 +0800

    some changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+const list = [{
+  title:'react',
+  url: 'https://www.google.com',
+author: 'robin',
+num_comments:3,
+points:4,
+objectID:0,
+},{
+  title:'redux',
+  url: 'https://www.google.com',
+author: 'sy',
+num_comments:2,
+points:5,
+objectID:1,
+}]
+
 function App() {
+
+  var helloWorld="Welcome some refresh magic al";
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +29,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div><h2>{helloWorld}</h2></div>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,8 +38,18 @@ function App() {
         >
           Learn React
         </a>
+        <div>{list.map(function(item){
+          return (
+            <div>
+          <span><a href={item.url}>{item.title}</a></span><br/>
+          <span>Author: {item.author}</span><br/>
+          <span>Number of comment: {item.num_comments}</span><br/>
+          <span>{item.points}</span></div>)
+        })}</div>
       </header>
+      
     </div>
+    
   );
 }
 
